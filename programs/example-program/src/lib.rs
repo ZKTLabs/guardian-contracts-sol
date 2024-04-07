c456786afe542616b57165d6c934708adaa56041
Lone G <gyllone1993@gmail.com>
2024-04-07 15:52:59 +0800

    update

--- programs/example-program/src/lib.rs
+++ programs/example-program/src/lib.rs
@@ -1,18 +1,11 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{TokenAccount, Token, Transfer, transfer};
 
 declare_id!("3BxNMJ7VSjtVkrDLDY93D214MmQPBEx9LvTXu5gKxJkX");
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Src Balance < LP Deposit Amount.")]
-    NotEnoughBalance,
-    #[msg("Pool Mint Amount < 0 on LP Deposit")]
-    NoPoolMintOutput,
-    #[msg("Trying to burn too much")]
-    BurnTooMuch,
-    #[msg("Not enough out")]
-    NotEnoughOut,
+    #[msg("Not in whitelist")]
+    NotInWhitelist,
 }
 
 #[program]
@@ -36,37 +29,9 @@ pub struct Swap<'info> {
         ],
         bump = compliance.bump,
         seeds::program = guardian_program::id(),
-        constraint = compliance.is_whitelist,
+        constraint = compliance.is_whitelist @ ErrorCode::NotInWhitelist,
     )]
     pub compliance: Box<Account<'info, guardian_program::state::Compliance>>,
     // swap accounts
-    pub pool_state: Box<Account<'info, PoolState>>,
-    /// CHECK: this is the authority for the pool
-    #[account(seeds=[b"authority", pool_state.key().as_ref()], bump)]
-    pub pool_authority: UncheckedAccount<'info>,
-    #[account(mut, has_one = owner)]
-    pub user_in: Box<Account<'info, TokenAccount>>,
-    #[account(mut, has_one = owner)]
-    pub user_out: Box<Account<'info, TokenAccount>>,
-    #[account(
-        mut,
-        constraint = vault_in.mint == user_in.mint,
-        constraint = vault_in.owner == pool_authority.key(),
-    )]
-    pub vault_in: Box<Account<'info, TokenAccount>>,
-    #[account(
-        mut,
-        constraint = vault_out.mint == user_out.mint,
-        constraint = vault_out.owner == pool_authority.key(),
-    )]
-    pub vault_out: Box<Account<'info, TokenAccount>>,
-    pub token_program: Program<'info, Token>,
-}
-
-// a pool without fee
-#[account]
-pub struct PoolState {
-    pub mint0: Pubkey,
-    pub mint1: Pubkey,
-    pub total_amount_minted: u64,
+    // ...
 }
